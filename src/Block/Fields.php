cea9699b4d70091e62d03ab71e33b281bc89ea96
Manuele Menozzi <manuele.menozzi@gmail.com>
2014-11-04 18:02:44 +0100

    Introduce blocco fields con template per tema base

--- src/Block/Fields.php
+++ src/Block/Fields.php
@@ -8,5 +8,58 @@
 
 class Webgriffe_TaxIdPro_Block_Fields extends Mage_Core_Block_Template
 {
+    public function getTaxCodeEmId()
+    {
+        return 'tax_code-em';
+    }
 
-} 
+    public function getTaxCodeId()
+    {
+        return 'tax_code';
+    }
+
+    public function getTaxCodeName()
+    {
+        return 'tax_code';
+    }
+
+    public function getVatNumberEmId()
+    {
+        return 'vat_number-em';
+    }
+
+    public function getVatNumberId()
+    {
+        return 'vat_number';
+    }
+
+    public function getVatNumberName()
+    {
+        return 'vat_number';
+    }
+
+    public function getCountryId()
+    {
+        return 'country';
+    }
+
+    public function getIsBusinessAddressId()
+    {
+        return 'is_business_address';
+    }
+
+    public function getIsBusinessAddressName()
+    {
+        return 'is_business_address';
+    }
+
+    public function getCompanyName()
+    {
+        return 'company';
+    }
+
+    public function getCompanyId()
+    {
+        return 'company';
+    }
+}
