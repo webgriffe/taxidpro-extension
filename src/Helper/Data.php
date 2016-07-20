5035d91506f661cf54c196823af2d70bb4bd975f
Marco Scortegagna <marco.scortegagna@hotmail.it>
2016-07-20 12:44:04 +0200

    Possibilità di rendere obbligatorio CF e PIVA per specifiche nazioni da admin Magento

--- src/Helper/Data.php
+++ src/Helper/Data.php
@@ -1,4 +1,18 @@
 <?php
 
 class Webgriffe_TaxIdPro_Helper_Data extends Mage_Core_Helper_Abstract
-{}
+{
+    const TAX_CODE_MANDATORY_COUNTIES = 'customer/address/specificcountry_taxcode';
+
+    const TAX_VAT_MANDATORY_COUNTRIES = 'customer/address/specificcountry_taxvat';
+    
+    public function getTaxCodeMandatoryCountries()
+    {
+        return explode(',', Mage::getStoreConfig(self::TAX_CODE_MANDATORY_COUNTIES));
+    }
+    
+    public function getTaxVatMandatoryCountries()
+    {
+        return explode(',', Mage::getStoreConfig(self::TAX_VAT_MANDATORY_COUNTRIES));
+    }
+}
